d3167a712f9727307e4482e932abce458130267b
Asif Kazmi <akazmi001@hotmail.com>
2020-11-08 22:19:55 -0500

    a

--- Level 4/Level 4/Program.cs
+++ Level 4/Level 4/Program.cs
@@ -6,7 +6,15 @@ namespace Level_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // This code would be more clear if the variables were named based on what they are storing.
+
+            Console.WriteLine("What kind of thing are we talking about?");
+            string a = Console.ReadLine(); // This is the item.
+            Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+            string b = Console.ReadLine(); // This is an adjective for the item.
+            string c = "Doom"; // This describes the items effect.
+            string d = "3000"; // This is the power level of the item.
+            Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
         }
     }
 }
